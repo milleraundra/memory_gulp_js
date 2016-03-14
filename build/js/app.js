9e6dba05e21f700ebf884f28a6026676616f02d7
aundra miller and torrence stratton <miller.aundra@gmail.com>
2016-03-14 16:34:49 -0700

    update memory.js

--- build/js/app.js
+++ build/js/app.js
@@ -92,13 +92,14 @@ var Card = require('./../js/Card.js').Card;
 var Player = require('./../js/Player.js').Player;
 
 $(document).ready(function(){
-  event.preventDefault();
-  $("#player_input").submit(function(event){
+  $("#player_form").submit(function(event){
+
   var player = $('input#player').val();
   console.log(player);
   var new_player = new Player(player);
   $("#player_input").hide();
   $("#card_display").show();
+  event.preventDefault();
   });
 
 
